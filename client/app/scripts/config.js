ade9cf176d5558de832ae663b6ce4b52bf4838fa
Valtteri Virtanen <valtteri.virtanen@ecom.fi>
2013-03-27 21:36:00 +0200

    Added some requirejs config

---
+++ client/app/scripts/config.js
@@ -0,0 +1,23 @@
+require.config({
+    // App will be initialized from main.js
+    deps: ['main'],
+    paths: {
+        jquery: '../components/jquery/jquery',
+        'socket.io-client': '../components/socket.io-client/lib/socket.io-client',
+        bacon: '../components/bacon/dist/Bacon',
+        underscore: '../components/underscore/underscore',
+        bootstrap: 'vendor/bootstrap'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery'],
+            exports: 'jquery'
+        },
+        underscore: {
+            exports: '_'
+        },
+        bacon: {
+            deps: ['jquery']
+        }
+    }
+});
---
+++ client/app/scripts/main.coffee
@@ -0,0 +1,4 @@
+require ['app', 'jquery', 'underscore', 'bacon', 'bootstrap'],  (app, $, bacon) ->
+    'use strict'
+    console.log(app)
+    console.log('Running jQuery %s', $().jquery)
