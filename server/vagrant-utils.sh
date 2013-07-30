bb8d56dc7b464ae05250c194ffa6887aa15cc4df
Valtteri Virtanen <valtteri.virtanen@ecom.fi>
2013-07-30 19:38:05 +0300

    Add debug-alias for debugging node

--- server/vagrant-utils.sh
+++ server/vagrant-utils.sh
@@ -6,6 +6,10 @@
 
 if [ ! -f /root/setup-utils ]
 then
+  # debug alias for running node in debug mode
+  echo "alias debug='DEBUG=sh:* PORT=5000 ./node_modules/.bin/coffee --nodejs debug app/app.coffee'" >> /home/vagrant/.bash_aliases
+  chown vagrant:vagrant /home/vagrant/.bash_aliases
+
   aptitude install -y ruby
   aptitude install -y rubygems1.8
   gem install foreman
