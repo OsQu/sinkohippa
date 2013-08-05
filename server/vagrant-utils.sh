d5fa1ac5839e60c20219c4a82a42354c59f42bcf
Valtteri Virtanen <valtteri.virtanen@ecom.fi>
2013-08-05 17:26:13 +0300

    Add git and webkit-devtools-agent to server-project

--- server/vagrant-utils.sh
+++ server/vagrant-utils.sh
@@ -10,6 +10,7 @@ then
   echo "alias debug='DEBUG=sh:* PORT=5000 ./node_modules/.bin/coffee --nodejs debug app/app.coffee'" >> /home/vagrant/.bash_aliases
   chown vagrant:vagrant /home/vagrant/.bash_aliases
 
+  aptitude install -y git
   aptitude install -y ruby
   aptitude install -y rubygems1.8
   gem install foreman
