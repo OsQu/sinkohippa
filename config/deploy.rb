b304dab2e4a003870c4c44cbf292f320799ba544
Oskari Virtanen <oskari@flowdock.com>
2014-03-29 23:44:13 +0200

    Remove useless stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -94,14 +94,4 @@ namespace :deploy do
   after :updated, :npm_dependencies
   after :updated, :build_client
   after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
 end
